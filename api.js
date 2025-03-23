200871e3f1449b0f0348ddd7061dc04d65400a71
branden love <brandenseventeen@gmail.com>
2025-03-22 19:11:05 -0700

    trying again

--- api.js
+++ api.js
@@ -9,3 +9,24 @@ router.get("/hello", (req, res) => res.send("Hello World!"));
 api.use("/api/", router);
 
 export const handler = serverless(api);
+
+import express from "express";
+import path from "path";
+import { handler } from "@netlify/functions";  // Import Netlify function handler
+
+const app = express();
+
+// Set the view engine to ejs
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+// Serve static files from the "public" directory
+app.use(express.static(path.join(__dirname, "public")));
+
+// Define your routes
+app.get("/", (req, res) => {
+  res.render("index", { title: "My App" });
+});
+
+// Export the handler for Netlify functions using ES modules syntax
+export { handler as netlifyHandler };
--- index.js
+++ index.js
@@ -6,7 +6,7 @@ import jikanjs from '@mateoaranda/jikanjs';
 import cors from "cors";
 import path from "path";
 import { fileURLToPath } from 'node:url';
-import { handler } from "@netlify/functions";
+
 
 const app = express();
 const port = 3000;
@@ -307,7 +307,7 @@ app.get("/about", (req,res) => {
   res.render("about.ejs");
 });
 
-export { handler as netlifyHandler };
+
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
